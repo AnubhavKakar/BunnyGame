0050896a97f7f5783482d15492bcb4134d30f0e2
AnubhavKakar <anubhavkakar@gmail.com>
2022-10-22 18:48:39 +0100

    fixed animation

--- main.py
+++ main.py
@@ -1,100 +1,189 @@
 import pygame
+import os
 
 pygame.init()
 
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('BunnyGame')
 
-pygame.display.set_caption('Bunny Game')
-
+#set framerate
 clock = pygame.time.Clock()
 FPS = 60
 
-value = 0
+#define game variables
+GRAVITY = 0.75
 
-moving = False
-
-velocity = 12
-
-x1 = 100
-y1 = 150
-
+#define player action variables
 moving_left = False
 moving_right = False
 
-BG = (166, 199, 222)
 
-def drawBg():
+#define colours
+BG = (144, 201, 120)
+RED = (255, 0, 0)
+
+def draw_bg():
     screen.fill(BG)
+    pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
+
+
 
 class Bunny(pygame.sprite.Sprite):
-    def __init__(self, x1, y1, scale):
+    def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
-        img =  [pygame.image.load('Images/movel_0.png'),
-                pygame.image.load('Images/movel_1.png'),
-                pygame.image.load('Images/movel_2.png'),
-                pygame.image.load('Images/movel_3.png')]
+        self.alive = True
+        self.char_type = char_type
+        self.speed = speed
+        self.direction = 1
+        self.vel_y = 0
+        self.jump = False
+        self.in_air = True
+        self.flip = True
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
         
-        self.image = img 
-        #pygame.transform.scale(img, ((img.get_width() * scale), (img.get_height() * scale)))
-        #self.rect = self.image[0].get_rect()
-        #self.rect.center = (x1, y1)
+        #load all images for the players
+        animation_types = ['Idle', 'Run', 'Jump']
+        for animation in animation_types:
+            #reset temporary list of images
+            temp_list = []
+            #count number of files in the folder
+            num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
+            for i in range(num_of_frames):
+                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png')
+                img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
 
-    #def draw(self):
-     #   screen.blit(self.image, (x1, y1))
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
 
 
-player1 = Bunny(x1, y1, 1)
+    def move(self, moving_left, moving_right):
+        #reset movement variables
+        dx = 0
+        dy = 0
+
+        #assign movement variables if moving left or right
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        #jump
+        if self.jump == True and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+        #apply gravity
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+
+        #check collision with floor
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+            self.in_air = False
+
+        #update rectangle position
+        self.rect.x += dx
+        self.rect.y += dy
+
+
+    def update_animation(self):
+        #update animation
+        ANIMATION_COOLDOWN = 100
+        #update image depending on current frame
+        self.image = self.animation_list[self.action][self.frame_index]
+        #check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        #if the animation has run out the reset back to the start
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+
+
+    def update_action(self, new_action):
+        #check if the new action is different to the previous one
+        if new_action != self.action:
+            self.action = new_action
+            #update the animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+
+
+player = Bunny('player', 200, 200, 3, 5)
+
+
 
 run = True
 while run:
-    #drawBg()
-    
-    
+
     clock.tick(FPS)
 
+    draw_bg()
+
+    player.update_animation()
+    player.draw()
+
+    #update player actions
+    if player.alive:
+        if player.in_air:
+            player.update_action(2)#2: jump
+        elif moving_left or moving_right:
+            player.update_action(1)#1: run
+        else:
+            player.update_action(0)#0: idle
+        player.move(moving_left, moving_right)
+
+
     for event in pygame.event.get():
-        
-        
+        #quit game
         if event.type == pygame.QUIT:
             run = False
-        
+        #keyboard presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_w and player.alive:
+                player.jump = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
 
-                
+
+        #keyboard button released
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
-                moving = False
                 moving_left = False
-                # print("Stopped moving left")
             if event.key == pygame.K_d:
-                moving = False
-                moving_left = False
-                # print("Stopped moving right")
-        
-        key_pressed_is = pygame.key.get_pressed()
- 
-        if key_pressed_is[pygame.K_a]:
-            x1 -= 10
-            moving = True
-        if key_pressed_is[pygame.K_d]:
-            x1 += 10
-            moving = True
+                moving_right = False
 
-        if moving:
-            value += 3
-        
-        if value >= len(player1.image):
-            value = 0
 
-        image = player1.image[value]
 
-        image = pygame.transform.scale(image, (100,100))
 
-        screen.blit(image, (x1, y1))
-
-        pygame.display.update()
-
-        screen.fill((0,0,0))
+    pygame.display.update()
 
 pygame.quit()
